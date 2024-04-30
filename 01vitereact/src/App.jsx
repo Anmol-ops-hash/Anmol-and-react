20a6b165c5417937ddb841895fd2d872afa882ff
Anmol <nannawareanmol@gmail.com>
2024-04-30 23:57:37 +0530

    custom rendering

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,14 @@
 import Chai from "./chai";
+
 function App() {
-  return <Chai />;
+  const username = "Anmol and code!";
+  return (
+    <>
+      <Chai />
+      <h1>Anmol and react {username}</h1>
+      <p>test para</p>
+    </>
+  );
 }
 
 export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+// import { jsx as _jsx } from "react/jsx-runtime.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  );
+}
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="http://google.com" target="_blank">
+    Visite google
+  </a>
 );
+
+const anotherUser = "anmol and react";
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to viste google",
+  anotherUser,
+  anotherElement
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
